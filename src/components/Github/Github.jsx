98d6b8cc0b8bc786e63c7768a6c22ea6f3be6e80
Vedant Rathavi <vedantrathavi2006@gmail.com>
2025-06-14 10:22:27 +0530

    updated

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,22 +1,30 @@
 import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Github = () => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/vedant1994").then((Response) =>
-      Response.json().then((data) => {
-        console.log(data);
-        setData(data);
-      })
-    );
-  }, []);
+  const data = useLoaderData();
+
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/vedant1994").then((Response) =>
+  //     Response.json().then((data) => {
+  //       console.log(data);
+  //       setData(data);
+  //     })
+  //   );
+  // }, []);
 
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
       Github follower: {data.followers}
-      <img src={data.avatar_url} alt="Git Photot" width={300} />
+      <img src={data.avatar_url} alt="Git Photo" width={300} />
     </div>
   );
 };
 
 export default Github;
+
+export const githubInfoforLoader = async () => {
+  const response = await fetch("https://api.github.com/users/vedant1994");
+  return response.json();
+};
