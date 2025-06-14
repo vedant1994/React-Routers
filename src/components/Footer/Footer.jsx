fd3973b7bc9ba14549e2b50faa9a0cf0138d9c4a
Vedant Rathavi <vedantrathavi2006@gmail.com>
2025-06-14 10:28:49 +0530

    Final

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -40,7 +40,7 @@ export default function Footer() {
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/hiteshchoudhary"
+                    href="https://github.com/vedant1994"
                     className="hover:underline"
                     target="_blank"
                     rel="noreferrer"
@@ -49,9 +49,14 @@ export default function Footer() {
                   </a>
                 </li>
                 <li>
-                  <Link to="/" className="hover:underline">
-                    Discord
-                  </Link>
+                  <a
+                    href="https://www.linkedin.com/in/vedantrathavi/"
+                    className="hover:underline"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Linkedin
+                  </a>
                 </li>
               </ul>
             </div>
@@ -78,10 +83,8 @@ export default function Footer() {
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center">
             © 2023
-            <a href="https://hiteshchoudhary.com/" className="hover:underline">
-              hiteshchoudhary
-            </a>
-            . All Rights Reserved.
+            <a className="hover:underline">vedantrathavi</a>. All Rights
+            Reserved.
           </span>
           <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
             <Link to="#" className="text-gray-500 hover:text-gray-900">
